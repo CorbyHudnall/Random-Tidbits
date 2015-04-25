2b1568efd614083a56f2ae36e49fa58291b9841c
Corby Hudnall <corby.hudnall@gmail.com>
2015-04-25 11:52:02 -0700

    Adding some powershell tidbits

---
+++ PowerShell/Create SHA1 file hash.ps1
@@ -0,0 +1,4 @@
+﻿$file = (Get-Item "C:\somefile.txt").FullName
+
+$sha1 = New-Object System.Security.Cryptography.SHA1CryptoServiceProvider 
+[System.BitConverter]::ToString( $sha1.ComputeHash([System.IO.File]::ReadAllBytes($file)))
---
+++ PowerShell/De-Dup-Files-In-Directory.ps1
@@ -0,0 +1,26 @@
+﻿$originDirectory = "C:\some\origin\folder\"
+$backupTargetDir = "C:\duplicates\"
+
+Get-ChildItem $originDirectory -File -Attributes !S+!H -Recurse | 
+    # Sort by LastWriteTime
+    Sort-Object LastWriteTime | 
+    # Group by Name, Length
+    Group-Object Name, Length | 
+    # Only Duplicate groups
+    Where-Object { $_.Count -gt 1 } |
+    # Select the duplicates
+    Foreach-Object { $_.Group[1..1000] } | 
+    # Move the file to the new consolidated location
+    ForEach-Object {
+        # Create new path
+        $targetFullPath = $_.FullName.Replace($originDirectory, $backupTargetDir);
+        $targetDir = $targetFullPath.TrimEnd($_.Name)
+
+        if (-not([System.IO.Directory]::Exists($targetDir))) {
+            [System.IO.Directory]::CreateDirectory($targetDir)
+        }
+
+        # Move the original file to the new location
+        $_.MoveTo($targetFullPath)
+        #$targetFullPath
+}
